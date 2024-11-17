411152a92fa83a7466b48c705bc25d4d9667c64e
sonyerim <sonyerim12@gmail.com>
2024-11-17 21:46:42 +0900

    [FEAT] 드로어 화면

--- app/src/main/java/com/example/playoke/MainPageActivity.kt
+++ app/src/main/java/com/example/playoke/MainPageActivity.kt
@@ -5,11 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.playoke.databinding.ActivityMainPageBinding
 
 class MainPageActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainPageBinding by lazy { ActivityMainPageBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_page)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
