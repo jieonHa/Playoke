9d9701376da6a513b0157b39d6a7a593efb84b8e
sonyerim <sonyerim12@gmail.com>
2024-11-22 19:40:55 +0900

    [FIX] 메인 화면 코드 이동

--- app/src/main/java/com/example/playoke/MainActivity.kt
+++ app/src/main/java/com/example/playoke/MainActivity.kt
@@ -1,38 +1,21 @@
 package com.example.playoke
 
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
-import com.example.playoke.databinding.ActivityMainBinding
-import com.google.android.material.bottomsheet.BottomSheetBehavior
-import com.google.android.material.bottomsheet.BottomSheetDialog
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.playoke.databinding.ActivityMainPageBinding
 
-class MainActivity : AppCompatActivity() {
+class MainPageActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainPageBinding by lazy { ActivityMainPageBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
-        val binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
-        /*binding.musicBottomSheetTrigger.setOnClickListener{
-            val dialog = BottomSheetDialog(this)
-            val view = layoutInflater.inflate(R.layout.fragment_bottom_sheet_layout_music, null)
-            val btnClose = view.findViewById<ImageButton>(R.id.idBtnDismiss)
-            btnClose.setOnClickListener{
-                dialog.dismiss()
-            }
-            dialog.setCancelable(false)
-            dialog.setContentView(view)
-            dialog.setOnShowListener {
-                val bottomSheet = dialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)
-                bottomSheet?.layoutParams?.height = ViewGroup.LayoutParams.MATCH_PARENT
-
-                val behavior = BottomSheetBehavior.from(bottomSheet!!)
-                behavior.state = BottomSheetBehavior.STATE_EXPANDED
-            }
-            dialog.show()
-
-        }*/
+        setSupportActionBar(binding.toolbar)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
