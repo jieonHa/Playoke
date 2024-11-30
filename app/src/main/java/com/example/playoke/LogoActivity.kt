e6360bec743ecc9d7a0a7c2d24f8332a2b01aa1f
jieonHa <windykay@sookmyung.ac.kr>
2024-11-30 18:38:06 +0900

    #2 [FEAT] 로고 화면 - 3초 띄우고 MainActivity로 화면 전환

--- app/src/main/java/com/example/playoke/LogoActivity.kt
+++ app/src/main/java/com/example/playoke/LogoActivity.kt
@@ -1,16 +1,24 @@
 package com.example.playoke
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class LogoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_logo)
+        GlobalScope.launch {
+            delay(3000)
+            startActivity(Intent(this@LogoActivity, MainActivity::class.java))
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
