491b3bd213cc46a6bba46386eeaa2c3c16a48c23
jieonHa <windykay@sookmyung.ac.kr>
2024-12-03 09:06:32 +0900

    #2 [FEAT] 로고화면

--- app/src/main/java/com/example/playoke/LogoActivity.kt
+++ app/src/main/java/com/example/playoke/LogoActivity.kt
@@ -1,11 +1,16 @@
 package com.example.playoke
 
+import android.Manifest
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -13,12 +18,34 @@ import kotlinx.coroutines.launch
 class LogoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_logo)
-        GlobalScope.launch {
-            delay(3000)
-            startActivity(Intent(this@LogoActivity, MainActivity::class.java))
+
+
+        lifecycleScope.launch {
+            delay(3000) // 3초 대기
+            if (arePermissionsGranted(this@LogoActivity)) {
+                // 권한이 허용된 경우 MainActivity로 이동
+                startActivity(Intent(this@LogoActivity, MainActivity::class.java))
+            } else {
+                // 권한이 허용되지 않은 경우 AccessPermission으로 이동
+                startActivity(Intent(this@LogoActivity, AccessPermission::class.java))
+            }
+            finish() // 현재 액티비티 종료
         }
+    }
+
+    private fun arePermissionsGranted(context: Context): Boolean {
+        val requiredPermissions = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.BLUETOOTH,
+            Manifest.permission.RECORD_AUDIO,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        return requiredPermissions.all { permission ->
+            ContextCompat.checkSelfPermission(context, permission) == PackageManager.PERMISSION_GRANTED
+        }
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
