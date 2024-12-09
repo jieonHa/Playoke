466f430bb6102d2385fcd805a0ec78a10e80753d
jieonHa <windykay@sookmyung.ac.kr>
2024-12-10 00:17:03 +0900

    [FEAT] #46 로그인 버튼 > 로그인 시 메인화면으로 전환

--- app/src/main/java/com/example/playoke/SignIn.kt
+++ app/src/main/java/com/example/playoke/SignIn.kt
@@ -1,6 +1,8 @@
 package com.example.playoke
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -12,11 +14,49 @@ class SignIn : AppCompatActivity() {
     lateinit var binding: ActivitySignInBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivitySignInBinding.inflate(layoutInflater)
-
         super.onCreate(savedInstanceState)
-
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+
+        binding.toSignUp.setOnClickListener{ // 회원가입으로 이동
+            val intent: Intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
+
+        binding.toIdPwFinding.setOnClickListener{ // 비밀번호 찾기로 이동
+            val intent: Intent = Intent(this, pwFinding::class.java)
+            startActivity(intent)
+        }
+
+        binding.signInBtn.setOnClickListener {
+            //이메일, 비밀번호 로그인.......................
+            val email: String = binding.email.text.toString()
+            val password: String = binding.pw.text.toString()
+            MyApplication.auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    binding.email.text.clear()
+                    binding.pw.text.clear()
+                    if (task.isSuccessful) {
+                        if (MyApplication.checkAuth()) {
+                            // 로그인 성공
+                            MyApplication.email = email
+                            val intent: Intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                        } else {
+// 발송된 메일로 인증 확인을 안 한 경우
+                            Toast.makeText(baseContext,
+                                "전송된 메일로 이메일 인증이 되지 않았습니다.",
+                                Toast.LENGTH_SHORT).show()
+                        }
+                    } else {
+                        Toast.makeText(baseContext, "로그인 실패",
+                            Toast.LENGTH_SHORT).show()
+                    }
+                }
+
+        }
+
+
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
