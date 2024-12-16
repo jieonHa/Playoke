686724c5835a497b5a00b6c396425e676766db89
jieonHa <windykay@sookmyung.ac.kr>
2024-12-16 17:48:41 +0900

    [FEAT] #51 비밀번호 재설정 기능 구현

--- app/src/main/java/com/example/playoke/pwReset.kt
+++ app/src/main/java/com/example/playoke/pwReset.kt
@@ -2,6 +2,7 @@ package com.example.playoke
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,17 +17,22 @@ class pwReset : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.pwResetBtn // 비밀번호 재설정 기능 추가
-
+        binding.pwResetBtn.setOnClickListener { // 비밀번호 재설정 기능 추가
+            val email:String = binding.emailForPw.text.toString().trim()
+            if (email.isEmpty()){
+                Toast.makeText(this, "이메일 주소를 입력해주세요", Toast.LENGTH_SHORT)
+            }
+            MyApplication.auth.sendPasswordResetEmail(email).addOnCompleteListener{ task ->
+                if (task.isSuccessful){
+                    binding.statusMsg.text = "$email 으로 비밀번호 재설정 메일이 전송되었습니다."
+                }else{
+                    binding.statusMsg.text = "Error: ${task.exception?.message}"
+                }
+            }
+        }
         binding.backToSignIn.setOnClickListener{
             val intent: Intent = Intent(this, SignIn::class.java)
             startActivity(intent)
         }
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
         }
-    }
 }
