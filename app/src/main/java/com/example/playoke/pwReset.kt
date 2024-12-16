4c6e2213b671bdac734b459a75dd4f8d23e0ca64
jieonHa <windykay@sookmyung.ac.kr>
2024-12-16 17:16:19 +0900

    [FEAT] #49 비밀번호 재설정 activity

--- app/src/main/java/com/example/playoke/pwReset.kt
+++ app/src/main/java/com/example/playoke/pwReset.kt
@@ -1,16 +1,28 @@
 package com.example.playoke
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.playoke.databinding.ActivityPwResetBinding
+import com.example.playoke.databinding.ActivitySignInBinding
 
 class pwReset : AppCompatActivity() {
+    lateinit var binding: ActivityPwResetBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityPwResetBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_pw_reset)
+        setContentView(binding.root)
+
+        binding.pwResetBtn // 비밀번호 재설정 기능 추가
+
+        binding.backToSignIn.setOnClickListener{
+            val intent: Intent = Intent(this, SignIn::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
